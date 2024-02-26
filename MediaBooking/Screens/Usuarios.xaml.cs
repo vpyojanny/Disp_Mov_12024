7076cd7f2abbd6f3be95213c321b5f7242632c15
omarurena <1988informatica@gmail.com>
2024-02-25 20:24:53 -0400

    Vista de usuarios completada (eso creo jajaaj)

--- MediaBooking/Screens/Usuarios.xaml.cs
+++ MediaBooking/Screens/Usuarios.xaml.cs
@@ -1,9 +1,70 @@
+using Android.Net.Wifi.Aware;
+using System.Collections.ObjectModel;
+
 namespace MediaBooking.Screens;
 
-public partial class Usuarios : ContentPage
+public partial class Usuarios : TabbedPage
 {
-	public Usuarios()
+
+    public ObservableCollection<Reservaciones> ListaReservaciones { get; set; }
+    public Usuarios()
 	{
-		InitializeComponent();
+        IniciarDatos();
+        InitializeComponent();
+
+        listaReservaciones.ItemsSource = ListaReservaciones;
+       
+        
+
+       
+    }
+
+
+	public class Reservaciones
+	{
+		public string Nombre { get; set; }
+		public string Correo { get; set; }
+
+		public string Telefono { get; set; }
+		public string Direccion { get; set; }
+		public string Rol { get; set; }
+
 	}
+
+	private void IniciarDatos()
+	{
+		ListaReservaciones = new ObservableCollection<Reservaciones>
+        {
+            new() {
+                Nombre = "Omar Urena",
+                Correo = "example@gmail.com",
+                Telefono = "8092692222",
+                Direccion = "Santiago",
+                Rol = "Administrador"
+
+            },
+            new ()
+            {
+                Nombre = "Miguel Tejada",
+                Correo = "example@gmail.com",
+                Telefono = "8092692222",
+                Direccion = "Santiago",
+                Rol = "Administrador"
+
+            },
+            new ()
+            {
+                Nombre = "Santo Susuki",
+                Correo = "example@gmail.com",
+                Telefono = "8092692222",
+                Direccion = "Santiago",
+                Rol = "Administrador"
+
+            }
+        };
+      
+
+    }
+
+
 }
