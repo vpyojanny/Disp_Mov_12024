c35db38b8615f02f38f682f63eb1c947d8e5207c
omarurena <1988informatica@gmail.com>
2024-02-25 22:00:29 -0400

    Revert "Pantalla de usuarios completada, chequeen el archivo App.xaml.cs, deje un comentario para mayor orientacion"
    This reverts commit 54b90408d6eb8932e200058687a8d04b62736193.

--- MediaBooking/App.xaml.cs
+++ MediaBooking/App.xaml.cs
@@ -6,8 +6,7 @@
         {
             InitializeComponent();
 
-            //Modificar este codigo a la ventana que quieren que se abra al compilar
-            MainPage = new Screens.Usuarios();
+            MainPage = new AppShell();
         }
     }
 }
