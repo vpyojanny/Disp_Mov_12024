87044e2c010b5cc002c4a0c64427aa0dd13a279d
omarurena <1988informatica@gmail.com>
2024-03-11 18:20:58 -0400

    Más cambios

---
+++ MediaBooking/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBooking.Models
+{
+    public class Usuario
+    {
+        public long Id_user { get; set; }
+        public string Nombre_user { get; set; }
+
+        public string Apellido_user { get; set; }
+
+        public string Correo_user { get; set; }
+
+        public string Telefono_user { get; set; }
+
+        public string Direccion_user { get; set; }
+
+        public string Contrasena_user { get; set; }
+    }
+}
